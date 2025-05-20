48bcd13945b0b27290059973f8cd7dad0ca4f8e1
Mustafa Sönmez <ms.sonmez40@gmail.com>
2025-05-20 18:06:51 +0000

    Errors

--- test/verifier_test.go
+++ test/verifier_test.go
@@ -55,10 +55,48 @@ func TestVerifier(t *testing.T) {
 			"Emptyinfo": []map[string]any{},
 		},
 	})
-	result := basic_api.VerifyBytes[L1](&body)
+	result, errs := basic_api.VerifyBytes[L1](&body)
+	if len(errs) != 0 {
+		fmt.Println(errs)
+		t.FailNow()
+	}
 	b, err := json.MarshalIndent(result, "", "  ")
 	if err != nil {
 		panic("ASDASDASD")
 	}
 	fmt.Println(string(b))
+}
+
+func TestVerifierInvalid(t *testing.T) {
+	mapData := []map[string]any{
+		{
+			"Name": "name",
+			"Info": map[string]any{
+				"Sub_info": []map[string]any{
+					{
+						"Sub_sub_info": 1,
+						"Flooaat":      1.2,
+						"Buul":         true,
+						"Listofnums":   []int{1, 2, 3},
+					},
+				},
+			},
+			"Moreinfo": []map[string]any{
+				{
+					"Sub_info": []map[string]any{
+						{
+							"Sub_sub_info": 1,
+							"Flooaat":      1.2,
+							"Buul":         false,
+							"Listofnums":   []any{1, 2, 3},
+						},
+					},
+				},
+			},
+			"Emptyinfo": []map[string]any{},
+		},
+	}
+	result, err := basic_api.VerifyList[L1](mapData)
+	fmt.Println(err)
+	fmt.Println(result)
 }
--- verifier.go
+++ verifier.go
@@ -5,67 +5,115 @@ import (
 	"encoding/json"
 	"fmt"
 	"reflect"
+	"strings"
 )
 
-func ParseNormal(val reflect.Value, type_ reflect.Type) reflect.Value {
-	returnVal := val.Convert(type_)
-	return returnVal
+type parseError struct {
+	Path   string
+	Reason string
 }
 
-func ParseStruct(val reflect.Value, type_ reflect.Type) reflect.Value {
-	var dataMap map[string]any = val.Interface().(map[string]any)
+type parser struct {
+	Errors []parseError
+}
+
+func (p *parser) AddError(path []string, reason string) {
+	err := parseError{
+		Path:   strings.Join(path, "."),
+		Reason: reason,
+	}
+	// DEBUG: panic(fmt.Sprintf("%v\n", err))
+	p.Errors = append(p.Errors, err)
+}
+
+func (p *parser) ParseNormal(val reflect.Value, type_ reflect.Type, path *[]string) reflect.Value {
+	for !val.CanConvert(type_) && (val.Kind() == reflect.Pointer || val.Kind() == reflect.Interface) {
+		val = val.Elem()
+	}
+	if !val.CanConvert(type_) {
+		p.AddError(*path, fmt.Sprintf("Value: %v %v can not be converted to Type: %v", val.Type(), val, type_))
+		return reflect.Zero(type_)
+	}
+	return val.Convert(type_)
+}
+
+func (p *parser) ParseStruct(val reflect.Value, type_ reflect.Type, path *[]string) reflect.Value {
+	dataMap, ok := val.Interface().(map[string]any)
+	if !ok {
+		p.AddError(*path, fmt.Sprintf("Value: %v %v can not be converted to Type: %v", val.Type(), val, type_))
+		return reflect.ValueOf(nil)
+	}
+
 	fields := reflect.VisibleFields(type_)
 	result := reflect.New(type_).Elem()
+
 	for _, fieldInfo := range fields {
 		fieldName := fieldInfo.Name
 		dataVal, exists := dataMap[fieldName]
-		if !exists { // do something?
+		if !exists { // TODO: Add to errors if nil not allowed.
 			continue
 		}
-		parsedVal := Parse(reflect.ValueOf(dataVal), fieldInfo.Type)
+		path_f := append(*path, fieldName)
+		parsedVal := p.Parse(reflect.ValueOf(dataVal), fieldInfo.Type, &path_f)
 		result.FieldByName(fieldName).Set(parsedVal)
 	}
 	return result
 }
 
-func ParseList(val reflect.Value, type_ reflect.Type) reflect.Value {
+func (p *parser) ParseList(val reflect.Value, type_ reflect.Type, path *[]string) reflect.Value {
+	if !isSlice(val.Type()) {
+		p.AddError(*path, fmt.Sprintf("Value: %v %v can not be converted to Type: %v", val.Type(), val, type_))
+		return reflect.ValueOf(nil)
+	}
 	resList := reflect.MakeSlice(type_, val.Len(), val.Len())
 	for i := range val.Len() {
 		elem := val.Index(i)
-		parsedVal := Parse(elem.Elem(), type_.Elem())
+		path_i := append(*path, fmt.Sprintf("[%d]", i))
+		parsedVal := p.Parse(elem, type_.Elem(), &path_i)
 		resList.Index(i).Set(parsedVal)
 	}
 	return resList
 }
 
-func Parse(val reflect.Value, type_ reflect.Type) reflect.Value {
+func (p *parser) Parse(val reflect.Value, type_ reflect.Type, path *[]string) reflect.Value {
 	switch type_.Kind() {
 	case reflect.Array, reflect.Slice:
-		val = ParseList(val, type_)
+		val = p.ParseList(val, type_, path)
 	case reflect.Struct:
-		val = ParseStruct(val, type_)
+		val = p.ParseStruct(val, type_, path)
 	default:
-		val = ParseNormal(val, type_)
+		val = p.ParseNormal(val, type_, path)
 	}
 	return val
 }
 
-func Verify[T any](mapData map[string]any) T {
-	res := Parse(reflect.ValueOf(mapData), reflect.TypeFor[T]())
-	return res.Interface().(T)
-}
+func Verify[T any](mapData map[string]any) (T, []parseError) {
+	p := parser{Errors: []parseError{}}
+	e := make([]string, 0)
+	res := p.Parse(reflect.ValueOf(mapData), reflect.TypeFor[T](), &e)
 
-func VerifyList[T any](mapData []map[string]any) []T {
-	result := make([]T, 0)
-	for _, i := range mapData {
-		result = append(result, Verify[T](i))
+	if len(p.Errors) > 0 {
+		return *new(T), p.Errors
 	}
-	return result
+	return res.Interface().(T), p.Errors
 }
 
-func VerifyBytes[T any](dataref *[]byte) any {
+func VerifyList[T any](mapData []map[string]any) ([]T, []parseError) {
+	result := make([]T, 0)
+	errList := make([]parseError, 0)
+	for _, i := range mapData {
+		parsedObj, errs := Verify[T](i)
+		result = append(result, parsedObj)
+		errList = append(errList, errs...)
+	}
+	// TODO: Fix parseError paths. They should start with [0] instead of nothing.
+	return result, errList
+}
+
+func VerifyBytes[T any](dataref *[]byte) (any, []parseError) {
 	data := *dataref
 	var result any
+	var errs []parseError
 	decoder := json.NewDecoder(bytes.NewReader(data))
 
 	if data[0] == '[' {
@@ -74,14 +122,14 @@ func VerifyBytes[T any](dataref *[]byte) any {
 		if err != nil {
 			fmt.Printf("ERRROR PARSING: %v\n", err)
 		}
-		result = VerifyList[T](jsonArr)
+		result, errs = VerifyList[T](jsonArr)
 	} else if data[0] == '{' {
 		jsonData := map[string]any{}
 		err := decoder.Decode(&jsonData)
 		if err != nil {
 			fmt.Printf("ERRROR PARSING: %v\n", err)
 		}
-		result = Verify[T](jsonData)
+		result, errs = Verify[T](jsonData)
 	}
-	return result.([]T)
+	return result.([]T), errs
 }
